c8298b972a110db05a4d7c409dd6d6305399779d
apple-yagi <applesryu@gmail.com>
2025-03-19 23:32:52 +0900

    Add biome configuration and update package.json scripts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
+import type { ESLint } from "eslint";
 import packageJson from "../package.json";
-import { ESLint } from "eslint";
 
 const plugin = {
-  meta: {
-    name: packageJson.name,
-    version: packageJson.version,
-  },
+	meta: {
+		name: packageJson.name,
+		version: packageJson.version,
+	},
 } satisfies ESLint.Plugin;
 
 export default plugin;
